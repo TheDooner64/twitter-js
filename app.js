d8df0c63a40f57f77050f622374c52466f6d21f0
TheDooner64 <Robert.Muldoon.1@gmail.com>
2015-11-10 13:39:44 -0500

    Set up server and home route

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+
+var port = 3000;
+
+app.listen(port, function() {
+    console.log("Listening on port " + port);
+});
+
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.path);
+    next();
+});
+
+app.get("/", function(req, res) {
+    // console.log(req.method);
+    res.send("Hello world using GET");
+});
+
+app.post("/", function(req, res) {
+    // console.log(req.method);
+    res.send("Hello world using POST");
+});
