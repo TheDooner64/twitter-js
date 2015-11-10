9cb110067399410db7bc60258196be252cec5ca3
everett980 <everett980@gmail.com>
2015-11-10 17:29:54 -0500

    Implement form submission feature on home page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var fs = require("fs");
 var tweetBank = require("../tweetBank");
 var path = require("path");
+var bodyParser = require('body-parser');
+
 
 router.use("/stylesheets/:fileName", function(req, res, next) {
     res.sendFile(path.join(__dirname.toString(), "../public/stylesheets", req.params.fileName.toString()), next);
@@ -10,12 +12,37 @@ router.use("/stylesheets/:fileName", function(req, res, next) {
 
 router.get("/", function(req, res) {
     var tweets = tweetBank.list();
-    res.render("index", { title: "Twitter.js", tweets: tweets } );
+    res.render("index", { showForm: true, title: "Twitter.js", tweets: tweets } );
 });
-   
-// router.post("/", function(req, res) {
-//     // console.log(req.method);
-//     res.send("Hello world using POST");
-// });
+
+router.get( '/users/:name', function(req, res, next) {
+  var name = req.params.name;
+  var tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: tweets } );
+});
+
+router.get( '/users/:name/tweets/:id', function(req, res, next) {
+	var name = req.params.name;
+	var id = req.params.id;
+	var tweets = tweetBank.find(req.params);
+  	res.render( 'index', { title: 'Twitter.js - Post by '+name, tweets: tweets } );
+});
+
+
+
+//Handle new tweets
+router.use(bodyParser.urlencoded( { extended : false }));
+router.use(bodyParser.json());
+
+// router.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   console.log(JSON.stringify(req.body, null, 2));
+//   res.end("null");
+// })
+
+router.post("/submit", function(req, res) {
+	tweetBank.add(req.body.name, req.body.text);
+	res.redirect('/');
+});
 
 module.exports = router;
--- tweetBank.js
+++ tweetBank.js
@@ -3,7 +3,7 @@ var _ = require("underscore");
 var data = [];
 
 var add = function (name, text) {
-  data.push({ name: name, text: text });
+  data.push({ name: name, text: text, id: data.length.toString() });
 };
 
 var list = function () {
@@ -33,4 +33,5 @@ var getFakeTweet = function() {
 
 for(var i=0; i<10; i++) {
   module.exports.add( getFakeName(), getFakeTweet() );
+  module.exports.add( "nimit", getFakeTweet() );
 }
